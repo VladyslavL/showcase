702a1baa2f2fec46be22b8225ff289cc61bc7000
VladyslavL <vladyslavliashenko@gmil.com>
2022-05-02 08:36:19 +0200

    removed Images

--- pages/old_index.js
+++ pages/old_index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
 	return (
@@ -16,7 +16,7 @@ export default function Home() {
 				<a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
 					Powered by{" "}
 					<span>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+						{/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
 					</span>
 				</a>
 			</footer>
